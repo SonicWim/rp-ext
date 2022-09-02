b1d615f187ba392c16a7fa236d4f7b8142b087ab
Wimpers <sonicwim@gmail.com>
2022-09-03 00:10:52 +0200

    More files
    Yes...

--- sata_sil/src/check-sata-sil.sh
+++ sata_sil/src/check-sata-sil.sh
@@ -2,9 +2,9 @@
 # Checking modules is loaded
 #
 
-echo -n "Loading module e1000e -> "
+echo -n "Loading module sata_sil -> "
 
-if [ `/sbin/lsmod |grep -i e1000e|wc -l` -gt 0 ] ; then
-        echo "Module e1000e loaded succesfully"
-        else echo "Module e1000e is not loaded "
+if [ `/sbin/lsmod |grep -i sata_sil|wc -l` -gt 0 ] ; then
+        echo "Module sata_sil loaded succesfully"
+        else echo "Module sata_sil is not loaded "
 fi
